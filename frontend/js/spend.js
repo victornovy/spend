26435a29fb53bb8b49c40706af538d2a1df41323
Victor Novy <novyvictor@gmail.com>
2016-08-11 00:21:37 -0300

    fix mobile layout | creating popup on new

--- frontend/js/spend.js
+++ frontend/js/spend.js
@@ -1,11 +1,68 @@
 var spendApp = angular.module('spendApp', ['ngMaterial']);
 
-spendApp.controller('MainCtrl', ['SpendService', function(spendService) {
+spendApp.controller('MainCtrl', ['$scope', 'SpendService', '$mdDialog', function($scope, spendService, $mdDialog) {
+
     var self = this;
 
-    self.spends = [];
+    $scope.spends = spendService.getSpends;
 
-    self.spends = spendService.getSpends;
+    self.openPopupNew = function(event) {
+        $mdDialog.show({
+            clickOutsideToClose: true,
+            preserveScope: true,
+            template: `<md-dialog>
+                          <md-dialog-content>
+                             Welcome to TutorialsPoint.com
+                             <form>
+                                <md-input-container>
+                                    <label>Bought to</label>
+                                    <input type="text"
+                                           ng-model="">
+                                    </input>
+                                </md-input-container>
+                                <md-input-container>
+                                    <label>Pricy</label>
+                                    <input type="text"
+                                           ng-model="">
+                                    </input>
+                                </md-input-container>
+                                <md-input-container>
+                                    <label>Acquired in</label>
+                                    <input type="text"
+                                           ng-model="">
+                                    </input>
+                                </md-input-container>
+                                <md-input-container>
+                                    <label>Type</label>
+                                    <input type="text"
+                                           ng-model="">
+                                    </input>
+                                </md-input-container>
+                                <md-input-container>
+                                    <label>Expiration Day</label>
+                                    <input type="text"
+                                           ng-model="">
+                                    </input>
+                                </md-input-container>
+                                <md-input-container>
+                                    <label>Description</label>
+                                    <input type="text"
+                                           ng-model="">
+                                    </input>
+                                </md-input-container>
+                             </form>
+                          </md-dialog-content>
+                      </md-dialog>`,
+            controller: function DialogController($scope, $mdDialog) {
+               debugger;
+            }
+        });
+    };
+
+    self.editRegister = function(index) {
+        var register = $scope.spends[index];
+        debugger;
+    };
 
 }]);
 
@@ -19,7 +76,8 @@ spendApp.factory('SpendService', [function() {
             "pricy": 49.99,
             "acquiredIn": "01/07/2016",
             "expirationDay": "17/07/2016",
-            "description": "test"
+            "typeOf": "Credit",
+            "description": "test test test test test"
         },
         {
             "id": 1234,
@@ -29,6 +87,29 @@ spendApp.factory('SpendService', [function() {
             "pricy": 9.99,
             "acquiredIn": "01/07/2016",
             "expirationDay": "17/07/2016",
+            "typeOf": "Debit",
+            "description": "Phone test"
+        },
+        {
+            "id": 12345,
+            "userId": 12345,
+            "boughtTo": "Victor Novy",
+            "productName": "Colomn",
+            "pricy": 60.00,
+            "acquiredIn": "06/07/2016",
+            "expirationDay": "06/07/2016",
+            "typeOf": "Debit",
+            "description": "Phone test"
+        },
+        {
+            "id": 123456,
+            "userId": 123456,
+            "boughtTo": "Victor Novy",
+            "productName": "Hair",
+            "pricy": 15.00,
+            "acquiredIn": "10/07/2016",
+            "expirationDay": "06/07/2016",
+            "typeOf": "Debit",
             "description": "Phone test"
         }
     ];
