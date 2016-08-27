7117b15065fb989acb45a8af2713c93b7b99203d
Victor Novy <novyvictor@gmail.com>
2016-08-27 11:07:49 -0300

    Created model

---
+++ models/SpendModel.js
@@ -0,0 +1,19 @@
+function SpendModel() {};
+
+SpendModel.prototype.find = function(query, callback) {
+
+};
+
+SpendModel.prototype.create = function(query, callback) {
+
+};
+
+SpendModel.prototype.update = function(_id, data, callback) {
+
+};
+
+SpendModel.prototype.remove = function(_id, callback) {
+
+};
+
+module.exports = new SpendModel();
