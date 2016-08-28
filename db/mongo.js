e8df290c6ad23215ff6c38c9769a3bbcd505104f
Victor Novy <novyvictor@gmail.com>
2016-08-28 12:03:14 -0300

    Created db connection

---
+++ db/mongo.js
@@ -0,0 +1,21 @@
+var mongojs = require('mongojs'),
+    config = require('config'),
+    debug = require('debug')('libro_nodejs:db');
+
+function _connection() {
+    var username = config.get('mongo.username'),
+        password = config.get('mongo.password'),
+        server   = config.get('mongo.server'),
+        port     = config.get('mongo.port'),
+        databse  = config.get('mongo.databse'),
+        auth     = !!username ? `${username} : ${password}` : '';
+
+    return `mongodb://${auth}${server}:${port}/${databse}`;
+}
+
+var db = mongojs(_connection);
+db.on('error', function(err) {
+    debug(err);
+});
+
+module.exports = db;
