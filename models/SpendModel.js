52b2ea9b955687319720bbaa3798bcbb51b25a18
Victor Novy <novyvictor@gmail.com>
2016-08-28 12:33:15 -0300

    Created model's integration

--- models/SpendModel.js
+++ models/SpendModel.js
@@ -1,19 +1,31 @@
-function SpendModel() {};
-
-SpendModel.prototype.find = function(query, callback) {
-
+function SpendModel(mongo) {
+    this.mongo = mongo;
+    this.spendCollection = this.mongo.collection('spends');
 };
 
-SpendModel.prototype.create = function(query, callback) {
+SpendModel.prototype.find = function(query, callback) {
+    this.spendCollection.find(query, callback);
+};
 
+SpendModel.prototype.findOne = function(_id, callback) {
+    var query = {_id: this.mongo.ObjectId(_id)};
+    this.spendCollection.findOne(query, callback)
+};
+
+SpendModel.prototype.create = function(data, callback) {
+    this.spendCollection.insert(data, callback);
 };
 
 SpendModel.prototype.update = function(_id, data, callback) {
-
+    var query = {_id: this.mongo.ObjectId(_id)};
+    this.spendCollection.update(query, data, callback);
 };
 
 SpendModel.prototype.remove = function(_id, callback) {
-
+    var query = {_id: this.mongo.ObjectId(_id)};
+    this.spendCollection.remove(query, callback);
 };
 
-module.exports = new SpendModel();
+module.exports = function(mongo) {
+    return new SpendModel(mongo);
+};
